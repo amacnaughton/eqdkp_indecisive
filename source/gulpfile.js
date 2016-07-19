1873644bf5b6ed6b1c9d0c8575261f8916948bb4
Andrew Macnaughton <andrew.macnaughton@theiggroup.com>
2016-07-19 02:12:33 +0100

    initial commit

---
+++ source/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  sass = require('gulp-sass'),
+  cssnano = require('gulp-cssnano'),
+  prefix = require('gulp-autoprefixer'),
+  sourcemaps = require('gulp-sourcemaps'),
+  jshint = require('gulp-jshint'),
+  stylish = require('jshint-stylish'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  plumber = require('gulp-plumber'),
+  bower = require('gulp-bower');
+
+gulp.task('css', function() {
+  gulp.src('./styles/**.*css')
+    .pipe(sass({
+      errLogToConsole: false
+    }))
+    .pipe(rename('eqdkp_indecisive.css'))
+    .pipe(gulp.dest('..'))
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('./scripts/*.js')
+    .pipe(plumber())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(concat('eqdkp_indecisive.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('..'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./styles/*/**', ['css']);
+  gulp.watch('./scripts/*/**', ['scripts']);
+});
+
+gulp.task('default', ['css', 'scripts', 'watch'], function() {});
